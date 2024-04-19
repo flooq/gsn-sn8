033e1bcd91cc509825a12df8dd52db0edea459f9
dzony <patryk@tensorflight.com>
2024-04-19 16:33:44 +0200

    docker files

---
+++ gsn/docker/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+IMAGE="gsn:sn8"
+CONTAINER="gsn-sn8"
+
+DOCKER_DIR=$(dirname "$(readlink -f "$0")")
+echo "Docker directory: $DOCKER_DIR"
+BASELINE_DIR=$(dirname "$DOCKER_DIR")
+echo "Baseline directory: $BASELINE_DIR"
+PROJ_DIR=$(dirname "$BASELINE_DIR")
+echo "Project directory: $PROJ_DIR"
+
+DATA_DIR="$PROJ_DIR/inputs/dataset"
+
+if [ ! -d "$DATA_DIR" ]; then
+    echo "Error: Dataset directory '$DATA_DIR' does not exist. Run 'python prepare_dataset.py' scripts first!!!"
+    exit 1
+fi
+
+docker run --runtime nvidia --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -it --rm \
+	-v "${PROJ_DIR}":/gsn-sn8 \
+	--name ${CONTAINER} \
+	${IMAGE} /bin/bash
