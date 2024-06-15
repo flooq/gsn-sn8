314b1979b5623c0fca347b994cdb9384c9b3a968
dzony <patryk@tensorflight.com>
2024-06-15 17:17:23 +0200

    fix division by zero

--- gsn/loss/metrics.py
+++ gsn/loss/metrics.py
@@ -7,7 +7,7 @@ def get_flood_segmentation_metrics(pred, gt_mask):
     tp = torch.sum(torch.logical_and(pred_mask > 0, pred_mask == gt_mask)).item()
     fp = torch.sum(torch.logical_and(pred_mask > 0, pred_mask != gt_mask)).item()
     fn = torch.sum(torch.logical_and(pred_mask == 0, gt_mask > 0)).item()
-    precision = tp / (tp + fp)
-    recall = tp / (tp + fn)
-    accuracy = tp / (tp + fp + fn)
+    precision = tp / (tp + fp) if tp + fp > 0 else 1
+    recall = tp / (tp + fn) if tp + fn > 0 else 1
+    accuracy = tp / (tp + fp + fn) if tp + fp + fn > 0 else 1
     return {"accuracy": accuracy, "precision": precision, "recall": recall}
