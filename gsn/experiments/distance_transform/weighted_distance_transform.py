9a715af463e0dc2ebaab17efe06e2f8d5d8d55e6
flooq <pawel.piotr.mackowski@gmail.com>
2024-08-14 21:11:44 +0200

    weighted distance transform inverted fix

--- gsn/experiments/distance_transform/weighted_distance_transform.py
+++ gsn/experiments/distance_transform/weighted_distance_transform.py
@@ -18,15 +18,50 @@ class WeightedDistanceTransform(nn.Module):
         for i in range(flood_np.shape[0]):  # iterate over batch
             for j in range(flood_np.shape[1]):  # iterate over the 4 masks
                 distance_transform = distance_transform_edt(flood_np[i, j])
-                if self.weights is not None:
-                    weight = self.weights[j]
-                    distance_transform *= weight
-                # Normalize distance transform
                 max_distance = distance_transform.max()
                 if max_distance > 0:
                     if self.inverted:
                         distance_transform = np.where(flood_np[i, j] > 0, max_distance - distance_transform, 0)
                     distance_transform /= max_distance
+                # change to [0, weight]
+                if self.weights is not None:
+                    weight = self.weights[j]
+                    distance_transform *= weight
                 distance_transforms[i, j] = distance_transform
         distance_transforms_tensor = torch.from_numpy(distance_transforms).to(flood_batch.device)
         return distance_transforms_tensor
+
+if __name__ == "__main__":
+    flood_batch = torch.tensor([[
+        [[0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0],
+         [0, 0, 0, 1, 1],
+         [0, 0, 1, 1, 1],
+         [0, 0, 1, 1, 1]],
+
+        [[0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0],
+         [0, 0, 0, 1, 1],
+         [0, 0, 1, 1, 1],
+         [0, 0, 1, 1, 1]],
+
+        [[0, 1, 1, 1, 0],
+         [1, 0, 0, 0, 1],
+         [1, 0, 0, 0, 1],
+         [1, 0, 0, 0, 1],
+         [0, 1, 1, 1, 0]],
+
+        [[0, 0, 0, 0, 0],
+         [0, 0, 1, 0, 0],
+         [0, 1, 1, 1, 0],
+         [0, 0, 1, 0, 0],
+         [0, 0, 0, 0, 0]]
+    ]], dtype=torch.float32)
+
+    wdt = WeightedDistanceTransform(weights=[2, 1, 1, 1], inverted=True)
+    result = wdt(flood_batch)
+
+    print("Input tensor:")
+    print(flood_batch)
+    print("\nDistance Transform Result:")
+    print(result)
